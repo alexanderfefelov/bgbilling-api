ccef0c8b148f91841398052cb7478e8c4c90e6bd
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-09-04 09:06:24 +0300

    Minor fixes

--- tools/install-scalaxb-snapshot.bat
+++ tools/install-scalaxb-snapshot.bat
@@ -2,7 +2,7 @@
 
 git clone https://github.com/eed3si9n/scalaxb.git
 pushd scalaxb
-sed -i "s/1.5.2/local-SNAPSHOT/" build.sbt
+sed -i "s/1.5.2/local-SNAPSHOT/g" build.sbt
 sbt "project app" "+ publishLocal"
-sbt "project ascalaxbPlugin" "+ publishLocal"
+sbt "project scalaxbPlugin" "+ publishLocal"
 popd
